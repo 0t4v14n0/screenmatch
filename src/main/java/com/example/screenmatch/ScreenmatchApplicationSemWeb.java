2c8a6a488c86d0aa63cc30b2d277596bc9a8929d
Otaviano Cruz Gouveia de Arruda Neto <0t4v14n0@gmail.com>
2024-06-14 00:53:21 -0300

    update

---
+++ src/main/java/com/example/screenmatch/ScreenmatchApplicationSemWeb.java
@@ -0,0 +1,28 @@
+package com.example.screenmatch;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+////import com.example.screenmatch.principal.Principal;
+//import com.example.screenmatch.principal.TestePrincipal;
+//import com.example.screenmatch.repository.SerieRepository;
+//
+//@SpringBootApplication
+//public class ScreenmatchApplicationSemWeb implements CommandLineRunner{
+//	
+//    @Autowired
+//    private SerieRepository repositorio;
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ScreenmatchApplicationSemWeb.class, args);
+//	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		TestePrincipal principal = new TestePrincipal(repositorio);
+//		principal.exibMenu();
+//	}
+//
+//}
